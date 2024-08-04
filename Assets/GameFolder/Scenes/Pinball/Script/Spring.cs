93af95d323043649df009b2b5832882f4cb93d75
bzick4 <bzick4@gmail.com>
2024-08-05 00:07:45 +0300

    spr+\-

--- Assets/GameFolder/Scenes/Pinball/Script/Spring.cs
+++ Assets/GameFolder/Scenes/Pinball/Script/Spring.cs
@@ -7,23 +7,23 @@ using UnityEngine.Animations;
 
 public class SpringStart : MonoBehaviour
 {
-    [SerializeField] private Transform targetPoint;
-    [SerializeField] private float speed, springForce;
-    [SerializeField] private GameObject launch, pointSpring;
+    [SerializeField] private Transform targetPoint;//for TransormTo
+    /// <summary>
+    /// for SpringTo
+    /// </summary>
+    [SerializeField] private float speed, springForce, damp;
+   // [SerializeField] private GameObject launch, launchPoint;
+    [SerializeField] private SpringJoint springJoint;
     public bool isStart { get; private set; }
-    private SpringJoint springJoint;
     
     private void Update()
     {
         Key();
-        TransormSpring();
     }
-
     public void StartLaunch()
     {
         isStart = true;
     }
-
     public void StopLaunch()
     {
         isStart = false;
@@ -31,38 +31,28 @@ public class SpringStart : MonoBehaviour
     
     public void SpringTo()
     {
-            springJoint = launch.GetComponent<SpringJoint>();
-            
-            springJoint.spring = springForce;
-            
-            Rigidbody pointSpringRb = pointSpring.GetComponent<Rigidbody>();
-            springJoint.connectedBody = pointSpringRb;
-            
-            springJoint.autoConfigureConnectedAnchor = false;
-            springJoint.connectedAnchor = Vector3.zero;
-            springJoint.anchor = Vector3.zero;
+        springJoint.spring = springForce;
+        springJoint.damper = damp;
     }
 
     private void TransormSpring()
     {
-        Vector3 direction = (targetPoint.position - transform.position).normalized;
+        springJoint.spring = 0f;
+        Vector3 direction = (targetPoint.position - transform.position).normalized; 
         transform.position += direction * speed * Time.deltaTime;
-        Debug.Log("Move ON");    
+        Debug.Log($"Move ON ");
     }
     public void Key()
     {
-        // if (Input.GetKeyDown(KeyCode.DownArrow))
-        // {
-        //     Debug.Log("нажал");
-        //     TransormSpring();
-        // }
-
-        if(Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Debug.Log($"нажал {springJoint.spring}");
+            TransormSpring();
+        }
+        if(Input.GetKeyUp(KeyCode.DownArrow))
         { 
-            Debug.Log("отпустил");
+            Debug.Log($"отпустил {springJoint.spring}");
             SpringTo();
         }
     }
-
-   
 }
